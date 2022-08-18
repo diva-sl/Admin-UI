5413e88541c4f9dd1c9c52702a0ee3e7b5ca0358
DivakaranS <divakaran.sivasamy@gmail.com>
2022-08-18 14:03:26 +0530

    5 files modified, 2 added

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 
-router.get('/admin', function(req, res, next) {
-	
-	res.render('admin', {page:req.query.page});
-
+router.get('/admin', function (req, res, next) {
+  res.render('admin', { page: req.query.page });
 });
-
-
 
 module.exports = router;
